31a152737d5c1f8a3fd51f8b14f843049a2d6e91
최수용 <dpfprtus@gmail.com>
2025-03-22 16:40:42 +0900

    [FEAT] Access Token 발급 API 스웨거 추가

--- src/main/java/com/example/kbuddy_backend/auth/controller/AuthController.java
+++ src/main/java/com/example/kbuddy_backend/auth/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.kbuddy_backend.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,14 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/kbuddy/v1/auth")
+@Tag(name = "Auth API", description = "인증 API 목록")
 @RequiredArgsConstructor
 public class AuthController {
 
 	private final AuthService authService;
 
 	@GetMapping("/accessToken")
+	@Operation(summary = "Access Token 발급", description = "쿠키에 포함된 Refresh Token을 통해 Access Token을 발급합니다.")
 	public ResponseEntity<AccessTokenResponse> getAccessToken(HttpServletRequest request) {
 		return ResponseEntity.ok().body(authService.getAccessToken(request));
 	}
