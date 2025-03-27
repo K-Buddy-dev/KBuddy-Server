6ed903c45acc04e772e64d7c0bf9c57e87a53f5b
CHONGMYOUNG LEE <dlchdaud321@gmail.com>
2025-03-27 15:30:41 +0900

    [CHORE] BlogController Response entity 수정

--- src/main/java/com/example/kbuddy_backend/blog/controller/BlogController.java
+++ src/main/java/com/example/kbuddy_backend/blog/controller/BlogController.java
@@ -33,9 +33,9 @@ public class BlogController {
 
     @PostMapping
     @Operation(summary = "블로그 게시글 작성", description = "새로운 블로그 게시글을 작성합니다.")
-    public ResponseEntity<DefaultResponse> saveBlog(@RequestBody BlogSaveRequest blogSaveRequest, @Parameter(hidden = true) @CurrentUser User user) {
+    public ResponseEntity<Void> saveBlog(@RequestBody BlogSaveRequest blogSaveRequest, @Parameter(hidden = true) @CurrentUser User user) {
         blogService.saveBlog(blogSaveRequest, user);
-        return ResponseEntity.ok(DefaultResponse.of(true, "블로그 작성 성공"));
+        return ResponseEntity.noContent().build(); // 204 No content 반환
     }
 
     @GetMapping
@@ -64,15 +64,16 @@ public class BlogController {
                                                       @RequestBody BlogUpdateRequest blogUpdateRequest,
                                                       @Parameter(hidden = true) @CurrentUser User user) {
         BlogResponse blog = blogService.updateBlog(blogId, blogUpdateRequest, user);
-        return ResponseEntity.ok().body(blog);
+        return ResponseEntity.ok(blog); // 200 OK, 수정된 블로그 데이터 반환
+        // ok().body(blog) -> ok(blog)로 변경: ok() 자체가 argument가 통과했을때 body값과 함께 응답하기 때문에 굳이 .body()를 사용할 필요가 없다.
     }
 
     @PostMapping("/{blogId}/images")
     @Operation(summary = "Blog 게시글 이미지 추가", description = "Blog 게시글에 이미지를 추가 합니다.")
-    public ResponseEntity<String> addBlogImages(@PathVariable final Long blogId, @RequestPart List<ImageFileDto> images,
+    public ResponseEntity<Void> addBlogImages(@PathVariable final Long blogId, @RequestPart List<ImageFileDto> images,
                                                @Parameter(hidden = true) @CurrentUser User user) {
         blogService.addImages(blogId, images, user);
-        return ResponseEntity.ok().body("이미지가 성공적으로 추가되었습니다.");
+        return ResponseEntity.noContent().build(); // 204 No Content 반환
     }
 
     @DeleteMapping("/{blogId}/images")
@@ -95,10 +96,10 @@ public class BlogController {
     // 단일 블로그 게시글을 북마크에 추가합니다.
     @PostMapping("/{blogId}/bookmark")
     @Operation(summary = "Blog 게시글 즐겨찾기", description = "Blog 게시글을 사용자 즐겨찾기 목록에 추가합니다.")
-    public ResponseEntity<String> addBookmark(@RequestBody BlogBookmarkRequest blogBookmarkRequest,
+    public ResponseEntity<Void> addBookmark(@RequestBody BlogBookmarkRequest blogBookmarkRequest,
                                               @PathVariable final Long blogId) {
         blogService.addBookmark(blogBookmarkRequest, blogId);
-        return ResponseEntity.ok().body("성공적으로 북마크 하였습니다.");
+        return ResponseEntity.noContent().build(); // 204 No Content 반환
     }
 
     // 단일 블로그 게시글을 북마크에서 제거합니다.
@@ -113,18 +114,18 @@ public class BlogController {
     // 블로그에 댓글을 추가합니다.
     @PostMapping("/{blogId}/comment")
     @Operation(summary = "댓글 작성", description = "블로그 게시글에 댓글을 작성합니다.")
-    public ResponseEntity<?> saveComment(@PathVariable Long blogId, @RequestBody BlogCommentSaveRequest blogCommentSaveRequest,
+    public ResponseEntity<Void> saveComment(@PathVariable Long blogId, @RequestBody BlogCommentSaveRequest blogCommentSaveRequest,
                                                        @Parameter(hidden = true) @CurrentUser User user) {
         blogCommentService.saveBlogComment(blogId, blogCommentSaveRequest, user);
-        return ResponseEntity.ok(DefaultResponse.of(true, "댓글 작성 성공"));
+        return ResponseEntity.noContent().build(); // 204 No Content 반환
     }
 
     // 블로그에 좋아요를 추가합니다.
     @PostMapping("/{blogId}/hearts")
     @Operation(summary = "Blog 게시글 좋아요", description = "Blog 게시글에 좋아요를 1개 올립니다.")
-    public ResponseEntity<?> plusBlogHeart(@PathVariable Long blogId, @Parameter(hidden = true) @CurrentUser User user) {
+    public ResponseEntity<Void> plusBlogHeart(@PathVariable Long blogId, @Parameter(hidden = true) @CurrentUser User user) {
         blogService.plusHeart(blogId, user);
-        return ResponseEntity.ok().body("블로그 좋아요 성공");
+        return ResponseEntity.noContent().build(); // 204 No Content 반환
     }
 
     // 블로그의 좋아요를 취소합니다.
@@ -138,9 +139,9 @@ public class BlogController {
     // 댓글에 좋아요를 추가합니다.
     @PostMapping("/{blogId}/{commentId}/hearts")
     @Operation(summary = "댓글 좋아요", description = "블로그 댓글에 좋아요를 1개 추가합니다.")
-    public ResponseEntity<?> plusCommentHeart(@PathVariable Long commentId, @Parameter(hidden = true) @CurrentUser User user) {
+    public ResponseEntity<Void> plusCommentHeart(@PathVariable Long commentId, @Parameter(hidden = true) @CurrentUser User user) {
         blogService.plusHeart(commentId, user);
-        return ResponseEntity.ok(DefaultResponse.of(true, "댓글 좋아요 성공"));
+        return ResponseEntity.noContent().build(); // 204 No Content 반환
     }
 
     // 댓글의 좋아요를 취소합니다.
@@ -163,12 +164,12 @@ public class BlogController {
     // 블로그를 신고합니다.
     @PostMapping("/{blogId}/report")
     @Operation(summary = "블로그 신고", description = "블로그 게시글을 신고합니다.")
-    public ResponseEntity<DefaultResponse> reportBlog(
+    public ResponseEntity<String> reportBlog(
             @PathVariable Long blogId,
             @RequestBody BlogReportRequest request,
             @Parameter(hidden = true) @CurrentUser User user) {
         blogService.reportBlog(blogId, request, user);
-        return ResponseEntity.ok(DefaultResponse.of(true, "신고 접수 성공"));
+        return ResponseEntity.noContent().build(); // 204 No Content 반환
     }
 
 //    @PatchMapping("/{blogId}/comment/{commentId}")
