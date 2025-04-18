8dfa07d3f74cb663f376aa6d199f009e2e51f8c0
CHONGMYOUNG LEE <dlchdaud321@gmail.com>
2025-04-18 10:30:55 +0900

    [DEL] 블로그 카테고리 관련  sortby enum 삭제

--- src/main/java/com/example/kbuddy_backend/blog/constant/SortBy.java
+++ src/main/java/com/example/kbuddy_backend/blog/constant/SortBy.java
@@ -5,11 +5,5 @@ public enum SortBy {
     HEART_COUNT,   // 좋아요 수 순
     COMMENT_COUNT, // 댓글 수 순
     LATEST,        // 최신순
-    OLDEST,         // 오래된 순
-
-    CATEGORY_VIEW_COUNT,
-    CATEGORY_HEART_COUNT,
-    CATEGORY_COMMENT_COUNT,
-    CATEGORY_LATEST,
-    CATEGORY_OLDEST
+    OLDEST         // 오래된 순
 } 
