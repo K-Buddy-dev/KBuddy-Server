2c008f9415e873e4b99b7a5ad1c66c9a4a269591
CHONGMYOUNG LEE <dlchdaud321@gmail.com>
2025-05-23 16:58:42 +0900

    [FEAT] 유저정보 조회에서 오는 id값을 uuid로 변경 - 유저 uuid 값과 비교해 본인의 글인지 판단하기 위함

--- src/main/java/com/example/kbuddy_backend/user/dto/response/UserResponse.java
+++ src/main/java/com/example/kbuddy_backend/user/dto/response/UserResponse.java
@@ -7,14 +7,14 @@ import com.example.kbuddy_backend.user.entity.User;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public record UserResponse(Long id, String userId, String email, List<String> roles,String profileImageUrl, String bio, String firstName,
+public record UserResponse(String uuid, String userId, String email, List<String> roles,String profileImageUrl, String bio, String firstName,
                            String lastName,
                            Gender gender, Country country, boolean isActive,LocalDateTime createdDate) {
 
-    public static UserResponse of(Long id, String userId, String email, List<String> roles, String profileImageUrl, String bio,
+    public static UserResponse of(String uuid, String userId, String email, List<String> roles, String profileImageUrl, String bio,
                                   String firstName, String lastName, LocalDateTime createdDate, Gender gender,
                                   Country country,boolean isActive) {
-        return new UserResponse(id, userId, email,roles, profileImageUrl, bio, firstName, lastName, gender, country,isActive,
+        return new UserResponse(uuid, userId, email,roles, profileImageUrl, bio, firstName, lastName, gender, country,isActive,
                 createdDate);
     }
 }
