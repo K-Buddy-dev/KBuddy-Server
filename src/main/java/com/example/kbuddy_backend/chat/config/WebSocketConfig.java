a510b4758b93d99d99066a43fed6691c5596200c
CHONGMYOUNG LEE <dlchdaud321@gmail.com>
2025-06-24 19:50:14 +0900

    [FEAT] 채팅 기능 구현(스프링 내부 브로커)

--- src/main/java/com/example/kbuddy_backend/chat/config/WebSocketConfig.java
+++ src/main/java/com/example/kbuddy_backend/chat/config/WebSocketConfig.java
@@ -15,15 +15,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // 메시지 브로커 설정
         // /topic 구독 주소 prefix
         // /app: 메시지 전송 주소 prefix
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+        config.enableSimpleBroker("/topic"); // "/topic"이 붙은 메시지를 발행할 경우, 메시지 브로커가 이를 처리
+        config.setApplicationDestinationPrefixes("/app"); // 메세지 핸들러로 라우팅도는 "/app"을 파라미터로 지정할 수 있다.
+        // 메시지 가공 처리가 필요할 경우, 가공 핸들러로 메시지를 라우팅 되도록하는 설정
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // 웹소켓 엔드포인트 설정
         // SockJS를 통한 웹소캣 연결 지원
-        registry.addEndpoint("/ws-chat")
+        // stomp 접속 주소 url = ws://localhost:8080/ws, 프로토콜이 http가 아님
+        registry.addEndpoint("/ws-chat") // 처음 웹소켓 HandShake를 위한 경로
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
--- src/main/java/com/example/kbuddy_backend/chat/dto/ChatMessage.java
+++ src/main/java/com/example/kbuddy_backend/chat/dto/ChatMessage.java
@@ -1,17 +1,19 @@
 package com.example.kbuddy_backend.chat.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ChatMessage {
     // 메시지 타입: 입장, 퇴장, 채팅
     public enum MessageType {
-        ENTER, LEAVE, CHAT
+        ENTER, LEAVE, TALK
     }
 
-    private MessageType type; // 메시지 타입
+    private MessageType messageType; // 메시지 타입
     private String roomId;    // 채팅방 ID
     private String sender;    // 메시지 발신자
     private String message;   // 메시지 내용
