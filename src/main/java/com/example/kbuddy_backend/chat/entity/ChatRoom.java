83eb64ce39031c48ea086edcd4d3f291bf44545d
CHONGMYOUNG LEE <dlchdaud321@gmail.com>
2025-08-12 13:14:07 +0900

    [FEAT] Repository 기능 확장(ChatMessageRepository) - 메시지 저장/조회 기능, 채팅방별 메시지 조회 기능

--- src/main/java/com/example/kbuddy_backend/chat/entity/ChatRoom.java
+++ src/main/java/com/example/kbuddy_backend/chat/entity/ChatRoom.java
@@ -7,35 +7,35 @@ import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
 
-//@Entity
-//@Getter
-//@Setter
-//public class ChatRoom {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @Column(nullable = false)
-//    private String roomId; // 채팅방 고유 ID, UUID를 사용하기 위해 String
-//
-//    @Column(nullable = false)
-//    private String name; // 채팅방 이름
-//
-//    @Column(nullable = false)
-//    private String counselorId; // 상담자 ID
-//
-//    @Column(nullable = false)
-//    private String clientId; // 내담자 ID
-//
-//    @Column(nullable = false)
-//    private LocalDateTime createdAt; // 생성 시간
-//
-//    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
-//    private List<ChatMessage> messages = new ArrayList<>();  // 채팅 메시지 목록
-//
-//    @PrePersist
-//    protected void onCreate() {
-//        createdAt = LocalDateTime.now();
-//    }
-//
-//}
+@Entity
+@Getter
+@Setter
+public class ChatRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String roomId; // 채팅방 고유 ID, UUID를 사용하기 위해 String
+
+    @Column(nullable = false)
+    private String name; // 채팅방 이름
+
+    @Column(nullable = false)
+    private String counselorId; // 상담자 ID
+
+    @Column(nullable = false)
+    private String clientId; // 내담자 ID
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt; // 생성 시간
+
+    @OneToMany(mappedBy = "chatRoom", cascade = CascadeType.ALL)
+    private List<ChatMessage> messages = new ArrayList<>();  // 채팅 메시지 목록
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+}
